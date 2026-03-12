9caa80b247da3742f0988fe02ae4caf3e0da9ad7
mako.kikvadze <mako.kikvadze@gmail.com>
2026-03-12 16:58:13 +0400

    fix secret code

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)rq$nx5c_7f-^ac+8n$97j5jj%l3jx1t4@9w@c+0&4y+b8qchi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-)rq$nx5c_7f-^ac+8n$97j5jj%l3jx1t4@9w@c+0&4y+b8qchi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
